b938453d5675f0b8e9b6151222e277465eb89cf5
Nicu Calcea <mail@nicu.md>
2023-09-25 10:14:15 +0100

    biz calendar update

--- R/sources/caa.R
+++ R/sources/caa.R
@@ -4,6 +4,7 @@ library(rvest)
 caa <- read_html("https://www.caa.co.uk/data-and-analysis/uk-aviation-market/data-publication-dates/") |>
   html_table() |>
   pluck(1) |>
+  janitor::row_to_names(row_number = 1) |>
   pivot_longer(-Date) |>
   filter(value != "") |>
   select(title = 2, date = 1) |>
