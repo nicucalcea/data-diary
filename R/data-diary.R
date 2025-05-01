0343e9c810e9b27fe48020097981ba344a941ea4
Nicu Calcea <mail@nicu.md>
2025-05-01 14:38:02 +0100

    may update

--- R/data-diary.R
+++ R/data-diary.R
@@ -69,13 +69,31 @@ source("R/sources/earnings.R")
 #################################################################
 
 # business_keywords <- c("agriculture in the united kingdom", "annual population survey", "apprenticeship", "balance sheet", "bank rate", "benefit sanctions", "budget allocation", "business", "capital acquisitons", "capital formation", "capital stocks", "claimant count", "construction output", "consumer price", "cost of living", "CPI(H)", "credit union", "domestic rates", "earnings and employment", "earnings and expenses", "economic activity", "Economic and fiscal outlook", "economic estimates", "economic statistics", "economic well-being", "electric vehicle", "electricity consumption", "employed people", "employee earnings", "employer skills survey", "employment cost index", "employment situation", "employment survey", "energy consumption in the UK", "energy performance of building certificates", "family food", "fiscal risk", "fuel prices", "foreign direct investment", "fuel sales", "gdp", "government debt", "gross domestic", "HICP", "house building", "house price", "household costs", "household energy efficiency", "household income", "housing benefit", "housing purchase affordability", "housing survey", "HMRC survey compliance cost", "import and export", "income estimates", "income from farming", "index of production", "index of services", "industrial turnover", "inflation euro area", "interest rate", "international reserves", "insolvency", "job openings", "job seekers", "kantar", "labour", "labor", "market data", "mergers and acquisitions", "money and credit", "national accounts", "pay gap", "price index", "pricing trends", "producer price", "productivity", "profitability", "public sector employment", "public sector finances", "rail fares", "rail passenger numbers", "rail performance", "real earnings", "rental prices", "rental sector", "retail sales", "revenues and expenses", "revenues and spend", "stamp duty", "taxpayers", "tax credit", "tax receipt", "tax relief", "time use", "trade", "trends and prices", "uk economic accounts", "uk energy in brief", "uk energy statistics", "union membership", "universal credit", "unemployment", "vehicle licensing statistics", "weekly earnings", "working and workless", "world economic outlook") |>
-  # paste(collapse = "|")
+# paste(collapse = "|")
 
 # important_keywords <- c("Bank of England Bank Rate", "Consumer price inflation, UK", "GDP monthly estimate", "GDP quarterly national accounts", "Retail sales", "UK labour market", "Public sector finances", "world economic outlook") |>
-  # paste(collapse = "|")
+# paste(collapse = "|")
 
-
-globalwitness_keywords <- c("climate", "environment", "renewable", "batter", "fossil", "petrol", "oil", "gas", "lng", "carbon", "emission", "green", "energy", "mining", "mines", "mineral", "commodit", "forest") |>
+globalwitness_keywords <- c(
+  "climate",
+  "environment",
+  "renewable",
+  "batter",
+  "fossil",
+  "petrol",
+  "oil",
+  "gas",
+  "lng",
+  "carbon",
+  "emission",
+  "green",
+  "energy",
+  "mining",
+  "mines",
+  "mineral",
+  "commodit",
+  "forest"
+) |>
   paste(collapse = "|")
 
 globalwitness_sources <- c("IEA", "Earnings")
@@ -86,9 +104,13 @@ globalwitness_sources <- c("IEA", "Earnings")
 #################################################################
 
 upcoming_stats <- gov_uk |>
-  filter(!title %in% c(ons$title
-                       # , nhs_digital$title, ofcom$title
-                       )) |>
+  filter(
+    !title %in%
+      c(
+        ons$title
+        # , nhs_digital$title, ofcom$title
+      )
+  ) |>
   mutate(source = "GOV.UK") |>
   bind_rows(ons |> mutate(source = "ONS")) |>
   bind_rows(nomis |> mutate(source = "Nomis")) |>
@@ -101,7 +123,9 @@ upcoming_stats <- gov_uk |>
   # bind_rows(caa |> mutate(source = "CAA")) |>
   # bind_rows(ofs |> mutate(source = "OFS")) |>
   # bind_rows(kantar |> mutate(source = "Kantar")) |>
-  bind_rows(eurostat |> mutate(source = "Eurostat", country = "European Union")) |>
+  bind_rows(
+    eurostat |> mutate(source = "Eurostat", country = "European Union")
+  ) |>
   bind_rows(bls |> mutate(source = "BLS", country = "United States")) |>
   bind_rows(un |> mutate(source = "UN", country = "International")) |>
   # bind_rows(oecd |> mutate(source = "OECD", country = "International")) |>
@@ -111,22 +135,41 @@ upcoming_stats <- gov_uk |>
   drop_na(date) |>
   filter(!grepl(" time series", title)) |>
   mutate(title_2 = title) |>
-  separate_wider_delim(cols = title_2, delim = ": ", names = c("title_1", "title_2"),
-                       too_few = "align_start", too_many = "drop") |>
+  separate_wider_delim(
+    cols = title_2,
+    delim = ": ",
+    names = c("title_1", "title_2"),
+    too_few = "align_start",
+    too_many = "drop"
+  ) |>
   group_by(date, title_1) |>
   slice_head(n = 1) |>
   ungroup() |>
-  select(-title_1, - title_2) |>
-  mutate(date = as.Date(date),
-         country = ifelse(is.na(country), "United Kingdom", country),
-         country = factor(country, levels = c("United Kingdom", "European Union", "United States", "International", "Corporate")),
-         flag = case_when(country == "European Union" ~ "🇪🇺",
-                          country == "International" ~ "🌍",
-                          country == "Corporate" ~ "💲",
-                          T ~ countrycode::countrycode(country, "country.name", "unicode.symbol")),
-         # important = grepl(important_keywords, title, ignore.case = T),
-         # business = grepl(business_keywords, title, ignore.case = T) | isTRUE(business),
-         globalwitness = grepl(globalwitness_keywords, title, ignore.case = T) | source %in% globalwitness_sources) |>
+  select(-title_1, -title_2) |>
+  mutate(
+    date = as.Date(date),
+    country = ifelse(is.na(country), "United Kingdom", country),
+    country = factor(
+      country,
+      levels = c(
+        "United Kingdom",
+        "European Union",
+        "United States",
+        "International",
+        "Corporate"
+      )
+    ),
+    flag = case_when(
+      country == "European Union" ~ "🇪🇺",
+      country == "International" ~ "🌍",
+      country == "Corporate" ~ "💲",
+      T ~ countrycode::countrycode(country, "country.name", "unicode.symbol")
+    ),
+    # important = grepl(important_keywords, title, ignore.case = T),
+    # business = grepl(business_keywords, title, ignore.case = T) | isTRUE(business),
+    globalwitness = grepl(globalwitness_keywords, title, ignore.case = T) |
+      source %in% globalwitness_sources
+  ) |>
   arrange(date, country, business, title)
 
 ##################################################################
@@ -165,21 +208,32 @@ upcoming_stats <- gov_uk |>
 ##----------------------------------------------------------------
 
 write_sheets_cal <- function() {
-
   calendar_google_sheets <- upcoming_stats |>
-    filter(date >= lubridate::floor_date(Sys.Date()),
-           # date < lubridate::ceiling_date(Sys.Date() %m+% months(1), "month"),
-           globalwitness) |>
-    mutate(title = gs4_formula(
-      paste0('=HYPERLINK("', link, '", "', title, '")')
-    )) |>
+    filter(
+      date >= lubridate::floor_date(Sys.Date()),
+      # date < lubridate::ceiling_date(Sys.Date() %m+% months(1), "month"),
+      globalwitness
+    ) |>
+    mutate(
+      title = gs4_formula(
+        paste0('=HYPERLINK("', link, '", "', title, '")')
+      )
+    ) |>
     select(flag, country, title, date) |>
     identity()
 
-  range_clear("https://docs.google.com/spreadsheets/d/1SAPy0tfzRN66ngdblNeEFhbGy8Q5V96C0DeC9qRo6Wc/edit#gid=891834841", sheet = "Full Schedule", range = "B4:F")
+  range_clear(
+    "https://docs.google.com/spreadsheets/d/1SAPy0tfzRN66ngdblNeEFhbGy8Q5V96C0DeC9qRo6Wc/edit#gid=891834841",
+    sheet = "Full Schedule",
+    range = "B4:F"
+  )
 
-  range_write("https://docs.google.com/spreadsheets/d/1SAPy0tfzRN66ngdblNeEFhbGy8Q5V96C0DeC9qRo6Wc/edit#gid=891834841", calendar_google_sheets, sheet = "Full Schedule", range = "B4")
-
+  range_write(
+    "https://docs.google.com/spreadsheets/d/1SAPy0tfzRN66ngdblNeEFhbGy8Q5V96C0DeC9qRo6Wc/edit#gid=891834841",
+    calendar_google_sheets,
+    sheet = "Full Schedule",
+    range = "B4"
+  )
 }
 
 write_sheets_cal()
@@ -189,15 +243,17 @@ write_sheets_cal()
 ##################################################################
 
 write_ical <- function() {
-
   format_event <- function(start, end, summary, description, tz = "GMT") {
-    template <-"BEGIN:VEVENT\nUID:%s\nDTSTAMP:%s\nDTSTART;VALUE=DATE:%s\nSUMMARY:%s\nDESCRIPTION:%s\nEND:VEVENT"
-    sprintf(template, uuid::UUIDgenerate(),
-            format(Sys.time(), "%Y%m%dT%H%M%SZ", tz = tz),
-            format(start, "%Y%m%d", tz = tz),
-            # format(end, "%Y%m%dT%H%M%SZ", tz = tz),
-            summary,
-            description)
+    template <- "BEGIN:VEVENT\nUID:%s\nDTSTAMP:%s\nDTSTART;VALUE=DATE:%s\nSUMMARY:%s\nDESCRIPTION:%s\nEND:VEVENT"
+    sprintf(
+      template,
+      uuid::UUIDgenerate(),
+      format(Sys.time(), "%Y%m%dT%H%M%SZ", tz = tz),
+      format(start, "%Y%m%d", tz = tz),
+      # format(end, "%Y%m%dT%H%M%SZ", tz = tz),
+      summary,
+      description
+    )
   }
 
   export_calendar <- function(df, file, tz = "BST") {
@@ -208,20 +264,29 @@ write_ical <- function() {
     open(f, "w")
     writeLines(header, con = f)
     invisible(lapply(1:nrow(df), \(i) {
-      ic_char <- format_event(start = df$date[i], end = df$date[i] + 1,
-                              summary = paste0(
-                                # ifelse(df$important[i], "❗️ ", ""),
-                                               df$flag[i], " ", df$title[i]),
-                              description = df$link[i],
-                              tz = tz)
+      ic_char <- format_event(
+        start = df$date[i],
+        end = df$date[i] + 1,
+        summary = paste0(
+          # ifelse(df$important[i], "❗️ ", ""),
+          df$flag[i],
+          " ",
+          df$title[i]
+        ),
+        description = df$link[i],
+        tz = tz
+      )
       writeLines(ic_char, con = f)
     }))
     writeLines(footer, con = f)
     close(f)
   }
 
-  export_calendar(upcoming_stats, file = "output/globalwitness_cal.ics", tz = "GMT")
-
+  export_calendar(
+    upcoming_stats,
+    file = "output/globalwitness_cal.ics",
+    tz = "GMT"
+  )
 }
 
 write_ical()
